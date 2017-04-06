effc7f9cfd7a1801d8948dbccd5b2afc73bf2d37
Charlotte Dumartin <folliked@gmail.com>
2017-04-06 22:51:48 +0200

    -

--- demo.php
+++ demo.php
@@ -8,6 +8,32 @@ use lepiaf\SerialPort\Parser\SeparatorParser;
 use lepiaf\SerialPort\Configure\TTYConfigure;
 use lepiaf\SerialPort\Exception\LogicException;
 
+$form = $_POST(['form']);
+$tab = json_decode($form);
+
+foreach ($tab as $row) {
+	$type = $row['type'];
+	$options = $row['options'];
+
+
+	switch ($type) {
+		case 'avancer':
+			avancer($pas);
+			break;
+		case 'mesure':
+			mesure($pas);
+			break;
+		case 'reculer':
+			reculer($pas);
+			break;
+		case 'goder':		
+			goder($pas);
+			break;
+		case 'photo':
+			photo($pas);				
+	}
+}
+
 try {
 	$serialPort = new SerialPort(new SeparatorParser(";"), new TTYConfigure());
 	$serialPort->open('/dev/ttyACM0');
